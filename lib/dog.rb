d85af0de7f2577f4cecc2679833380590724e7a0
Griffin <gmd298@gmail.com>
2022-05-23 17:45:59 -0400

    commit tree dfa1d688e34e4d22010fd69a230208ccd0af2fc7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,49 @@
 class Dog
+  attr_accessor :name, :breed, :id
+  def initialize(name:, breed:, id: nil)
+    @id = nil
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE IF EXISTS dogs
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+  end
+
+  def self.new_from_db(row)
+    id, name, breed = row
+    self.new.tap {|s| s.id = id ; s.name = name ; s.breed = breed }
+  end
 
 end
